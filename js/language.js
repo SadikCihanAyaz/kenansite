4afe32551b8b3c1da8f3e40d92761f4e3b25eda8
BaharAyaz <baharacariu@gmail.com>
2022-04-20 22:32:55 +0300

    son geliştirme

--- js/language.js
+++ js/language.js
@@ -23,7 +23,7 @@ if(lang=='tr'){/////////////////////////////TR//////////////////////////////////
     strings["Client_Detail3"]                          = "RESTORASYON";
     strings["Client_Detail4"]                          = "BİNA VE DEKORASYON İŞLERİ";
     strings["Client_Detail5"]                          = "MİMARLIK VE MÜHENDİSLİK HİZMETLERİ VE PROJE GELİŞTİRME";
-    strings["Client_Detail6"]                          = "GAYRÜMENKUL PAZARLAMA(REAL ESTATE)";
+    strings["Client_Detail6"]                          = "GAYRiMENKUL PAZARLAMA(REAL ESTATE)";
     strings["AboutUsDetail"]              = "Türkiye’nin köklü ve itibarlı büyük işletmelerinden biri olma hedefiyle inşaat  , mühendislik - mimarlık hizmetleri , restorasyon  ve gayrimenkul pazarlama sektörlerinde hizmet vermektedir.";
     strings["AboutUsDetail1"] ="En karmaşık projeleri başarıyla üstlenebilecek ve hayata geçirebilecek durumda olan bir ekip anlayışıyla hareket eden acaryap inşaat ; Odak noktası müşterileri, çalışanları, tedarikçileri ve toplum olan şirket, sahip olduğu bilgi birikimi, beceri ve deneyimi sayesinde piyasada yektin bir ünvana sahiptir.";
     strings["AboutUsDetail2"] ="Sektörde uzmanlaşmış ekibiyle sıfır kaza ve hata oranıyla tüm projeleri hayata geçirmeye kararlıdır ve yeni iş alanlarında yatırım yaparak paydaşları için daha fazla değer yaratacak kapsamlı teknik bilgiyi istikrarla kullanmaya devam etmektedir.";
@@ -47,23 +47,23 @@ strings["OurWorks"]                               = "Our Works";
 strings["wedo"]                                   = "WE DO REAL ESTATE";
 strings["AboutUsDetail"]                          = "WE DO REAL ESTATE";
 strings["ManinPage"]                             = "Main Page";
-strings["Client_Detail1"]                          = "MÜHENDİSLİK YAPILARI";
-strings["Client_Detail2"]                          = "ENDÜSTRİYEL TESİSLER";
-strings["Client_Detail3"]                          = "RESTORASYON";
-strings["Client_Detail4"]                          = "BİNA VE DEKORASYON İŞLERİ";
-strings["Client_Detail5"]                          = "MİMARLIK VE MÜHENDİSLİK HİZMETLERİ VE PROJE GELİŞTİRME";
-strings["Client_Detail6"]                          = "GAYRÜMENKUL PAZARLAMA(REAL ESTATE)";
-strings["Client_Area"]                          = "FAALİYET ALANLARIMIZ";
+strings["Client_Detail1"]                          = "ENGINEERING STRUCTURES";
+strings["Client_Detail2"]                          = "INDUSTRIAL FACILITIES";
+strings["Client_Detail3"]                          = "RESTORATION";
+strings["Client_Detail4"]                          = "BUILDING AND DECORATION WORKS";
+strings["Client_Detail5"]                          = "ARCHITECTURE AND ENGINEERING SERVICES AND PROJECT DEVELOPMENT";
+strings["Client_Detail6"]                          = "REAL ESTATE MARKETING";
+strings["Client_Area"]                          = "OUR ACTIVITY AREAS";
 strings["AboutUs1"]                                = "About Us";
 strings["Contact"]                              = "CONTACT INFORMATION";
 strings["FullName"]                              = "Full Name";
 strings["Email"]                              = "Email";
 strings["Address"]                              = "Address";
 strings["PhoneNumber"]                           = "Phone Number";
-strings["AboutUsDetail"]              = "Türkiye’nin köklü ve itibarlı büyük işletmelerinden biri olma hedefiyle inşaat  , mühendislik - mimarlık hizmetleri , restorasyon  ve gayrimenkul pazarlama sektörlerinde hizmet vermektedir.";
-strings["AboutUsDetail1"] ="En karmaşık projeleri başarıyla üstlenebilecek ve hayata geçirebilecek durumda olan bir ekip anlayışıyla hareket eden acaryap inşaat ; Odak noktası müşterileri, çalışanları, tedarikçileri ve toplum olan şirket, sahip olduğu bilgi birikimi, beceri ve deneyimi sayesinde piyasada yektin bir ünvana sahiptir.";
-strings["AboutUsDetail2"] ="Sektörde uzmanlaşmış ekibiyle sıfır kaza ve hata oranıyla tüm projeleri hayata geçirmeye kararlıdır ve yeni iş alanlarında yatırım yaparak paydaşları için daha fazla değer yaratacak kapsamlı teknik bilgiyi istikrarla kullanmaya devam etmektedir.";
-strings["AboutUsDetail3"] ="Geniş perspektif vizyonu anlayışıyla ve yeni teknolojik araçları kullanma yöntemleriyle yurtiçinde ve yurtdışında yeni projelere öncülük etmeyi hedeflemiş kurumsal bir dünya şirketi ilkesini benimsemiştir.";
+strings["AboutUsDetail"]              = "It provides services in the construction, engineering-architectural services, restoration and real estate marketing sectors with the aim of becoming one of Turkey's well-established and reputable large enterprises.";
+strings["AboutUsDetail1"] ="Acting with the understanding of a team that can successfully undertake and implement the most complex projects, acaryap İnşaat; Focusing on its customers, employees, suppliers and society, the company has a competent reputation in the market thanks to its knowledge, skills and experience.";
+strings["AboutUsDetail2"] ="It is determined to implement all projects with zero accident and error rate with its team specialized in the sector, and continues to consistently use comprehensive technical knowledge that will create more value for its stakeholders by investing in new business areas.";
+strings["AboutUsDetail3"] ="It has adopted the principle of a corporate world company, aiming to pioneer new projects at home and abroad with its broad perspective vision and methods of using new technological tools.";
 
 }
 
